8b3cd76e89611ae4f8583e289e40898adc76ace8
ouissal rms <khramswissal@gmail.com>
2025-12-04 20:23:08 +0100

    update now

--- product/src/App.js
+++ product/src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import Product from './Product';
 
 function App() {
+  const [product,setproduct] =useState([]);
+
+  const getProduct =()=>fetch('/Product.json')
+  .then(Response=>Response.json())
+  .then(data => setproduct(data))
+
+
+
+ useEffect(()=>{
+   getProduct();
+  },[])
+
+  const resultat = product.map((index , key) =>(
+    <Product product={index} key={key} />
+  )
+  )
+
   return (
     <div className="App">
      <h1> Products </h1>
+   <div>
+    {resultat}
+   </div>
+     
     </div>
   );
 }
